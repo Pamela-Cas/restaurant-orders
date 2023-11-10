da4e1f0411bcf860fdd60acf09d0766745e3cbb8
Pamela-Cas <pxcx611@gmail.com>
2023-11-10 12:16:37 -0300

    erro linter

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -12,10 +12,9 @@ def test_ingredient():
     assert ingredient_one == ingredient_two
     assert ingredient_one != ingredient_three
 
-    assert ingredient_three.name == "camarão"
-
     assert hash(ingredient_one) == hash(ingredient_two)
     assert hash(ingredient_one) != hash(ingredient_three)
+    assert ingredient_three.name == "camarão"
 
     assert ingredient_one.__eq__(ingredient_two) is True
     assert ingredient_one.__eq__(ingredient_three) is False
