024c6034787379077ae65c7928c2632633a5c21b
Pamela-Cas <pxcx611@gmail.com>
2023-11-10 09:39:38 -0300

    formantanção req 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -9,13 +9,19 @@ def test_ingredient():
     ingredient_two = Ingredient("manteiga")
     ingredient_three = Ingredient("queijo_mussarela")
 
+    assert ingredient_one == ingredient_two
+    assert ingredient_one != ingredient_three
+
+    assert ingredient_three.name == "queijo_mussarela"
+
     assert hash(ingredient_one) == hash(ingredient_two)
     assert hash(ingredient_one) != hash(ingredient_three)
+
     assert ingredient_one.__eq__(ingredient_two) is True
     assert ingredient_one.__eq__(ingredient_three) is False
-    assert ingredient_one == ingredient_two
-    assert ingredient_one.name == "manteiga"
+
     assert repr(ingredient_three) == "Ingredient('queijo mussarela')"
+
     assert ingredient_three.restrictions == {
         Restriction.LACTOSE,
         Restriction.ANIMAL_DERIVED,
