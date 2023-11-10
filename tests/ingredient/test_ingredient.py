77c2e18bc145c31df8bc8f9a3b9426f3c4c3ee45
Pamela-Cas <pxcx611@gmail.com>
2023-11-10 09:25:03 -0300

    force commit

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,22 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+    )
 
 
-# Req 1
 def test_ingredient():
-    pass
+    ingredient_one = Ingredient("manteiga")
+    ingredient_two = Ingredient("manteiga")
+    ingredient_three = Ingredient("queijo_mussarela")
+
+    assert hash(ingredient_one) == hash(ingredient_two)
+    assert hash(ingredient_one) != hash(ingredient_three)
+    assert ingredient_one.__eq__(ingredient_two) is True
+    assert ingredient_one.__eq__(ingredient_three) is False
+    assert ingredient_one == ingredient_two
+    assert ingredient_one.name == "manteiga"
+    assert repr(ingredient_three) == "Ingredient('queijo mussarela')"
+    assert ingredient_three.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
